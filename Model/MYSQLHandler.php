5cd900e02dbb731a965a06b5633fc21aa894a562
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-01-31 14:10:56 +0200

    admin part

--- Model/MYSQLHandler.php
+++ Model/MYSQLHandler.php
@@ -1,5 +1,6 @@
 <?php
-class MYSQLHandler implements DbHandler{
+class MYSQLHandler implements DbHandler
+{
 
     private $_db_handler;
     private $_table;
@@ -13,46 +14,40 @@ class MYSQLHandler implements DbHandler{
     public function connect()
     {
         $handler = mysqli_connect(__HOST__, __USER__, __PASS__, __DB__);
-        
+
         // Check connection
-        if($handler){
-            $this->_db_handler=$handler;
+        if ($handler) {
+            $this->_db_handler = $handler;
             return true;
         }
         return false;
     }
 
-    public function get_data($fields = array(),  $start = 0)
+    public function get_data($fields = array(), $start = 0)
     {
         $table = $this->_table;
-        if(empty($fields))
-        {
-            $sql="select * from " . $table;
-        }
-        else
-        {
-            $sql="select ";
-            foreach($fields as $f)
-            {
+        if (empty($fields)) {
+            $sql = "select * from " . $table;
+        } else {
+            $sql = "select ";
+            foreach ($fields as $f) {
                 $sql .= "`$f` ,";
             }
             $sql .= " from `$table`";
-            $sql = str_replace(", from", "from",$sql);
+            $sql = str_replace(", from", "from", $sql);
         }
-        $sql .= " limit $start," .__RECORDS_PER_PAGE__;
+        $sql .= " limit $start," . __RECORD_PER_PAGE__;
         return $this->get_results($sql);
     }
 
-
     public function disconnect()
     {
-        if($this->_db_handler)
-        {
+        if ($this->_db_handler) {
             mysqli_close($this->_db_handler);
         }
-    }   
-    
-    public function get_record_by_id($id,$primary_key)
+    }
+
+    public function get_record_by_id($id, $primary_key)
     {
         $table = $this->_table;
         $sql = "select * from `$table` where `$primary_key` = $id";
@@ -62,20 +57,16 @@ class MYSQLHandler implements DbHandler{
     private function get_results($sql)
     {
         $this->debug($sql);
-        $_handler_result = mysqli_query($this->_db_handler,$sql);
+        $_handler_result = mysqli_query($this->_db_handler, $sql);
         $_arr_result = array();
-   
-        if($_handler_result)
-        {
-            while($row = mysqli_fetch_array($_handler_result,MYSQLI_ASSOC)) 
-            {
-                 $_arr_result[]= array_change_key_case($row);
+
+        if ($_handler_result) {
+            while ($row = mysqli_fetch_array($_handler_result, MYSQLI_ASSOC)) {
+                $_arr_result[] = array_change_key_case($row);
             }
-                $this->disconnect();
-                return $_arr_result;
-       }
-        else
-        {
+            $this->disconnect();
+            return $_arr_result;
+        } else {
             $this->disconnect();
             return false;
         }
@@ -84,36 +75,31 @@ class MYSQLHandler implements DbHandler{
 
     public function search($column, $column_value)
     {
-        $table=$this->_table;
-        $sql = "select * from `$table` where `$column` like '%".$column_value."%'";
+        $table = $this->_table;
+        $sql = "select * from `$table` where `$column` like '%" . $column_value . "%'";
         return $this->get_results($sql);
     }
 
     public function save($new_value)
     {
-        if(is_array($new_value))
-        {
-            $table=$this->_table;
-            $sql1= "insert into `$table` (";
-            $sql2="values (";
-            foreach ($new_value as $key => $value) 
-            {
+        if (is_array($new_value)) {
+            $table = $this->_table;
+            $sql1 = "insert into `$table` (";
+            $sql2 = "values (";
+            foreach ($new_value as $key => $value) {
                 $sql1 .= "`$key` ,";
                 $sql2 .= "`$value` ,";
             }
-            $sql1= $sql1.")";
-            $sql2= $sql2.")";
-            $sql1= str_replace(",)",")",$sql1);
-            $sql2= str_replace(",)",")",$sql2);
+            $sql1 = $sql1 . ")";
+            $sql2 = $sql2 . ")";
+            $sql1 = str_replace(",)", ")", $sql1);
+            $sql2 = str_replace(",)", ")", $sql2);
             $sql = $sql1 . $sql2;
             $this->debug($sql);
-            if(mysqli_query($this->_db_handler,$sql))
-            {
+            if (mysqli_query($this->_db_handler, $sql)) {
                 $this->disconnect();
                 return true;
-            }
-            else
-            {
+            } else {
                 $this->disconnect();
                 return false;
             }
@@ -123,30 +109,24 @@ class MYSQLHandler implements DbHandler{
 
     private function debug($sql)
     {
-        if(__DEBUG_MODE__ ===1)
-        {
-            echo "<h5> Sent Query </h5>" .$sql . "<br>";
+        if (__DEBUG_MODE__ === 1) {
+            echo "<h5> Sent Query </h5>" . $sql . "<br>";
         }
     }
 
     public function get_full_data($fields = array())
     {
         $table = $this->_table;
-        if(empty($fields))
-        {
-            $sql="select * from " . $table;
-        }
-        else
-        {
-            $sql="select ";
-            foreach($fields as $f)
-            {
+        if (empty($fields)) {
+            $sql = "select * from " . $table;
+        } else {
+            $sql = "select ";
+            foreach ($fields as $f) {
                 $sql .= "`$f` ,";
             }
             $sql .= " from `$table`";
-            $sql = str_replace(", from", "from",$sql);
+            $sql = str_replace(", from", "from", $sql);
         }
         return $this->get_results($sql);
     }
 }
-?>
--- Views/admin/user.php
+++ Views/admin/user.php
@@ -1,2 +1,70 @@
 <?php
+$current_index = isset($_GET["id"]) && is_numeric($_GET["id"]) ? $_GET["id"] : 0;
+$items = $db->get_record_by_id($current_index, 'user_id');
+
 ?>
+<html>
+<style>
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+
+section {
+  display: -webkit-flex;
+  display: flex;
+}
+
+nav {
+  -webkit-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  background: #ccc;
+  padding: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  -webkit-flex: 3;
+  -ms-flex: 3;
+  flex: 3;
+  background-color: #f1f1f1;
+  padding: 10px;
+}
+
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+
+@media (max-width: 600px) {
+  section {
+    -webkit-flex-direction: column;
+    flex-direction: column;
+  }
+}
+</style>
+<table cellspacing="10">
+  <?php
+foreach ($items as $item) {
+    echo "<header>" . $item["name"] . "</header>";
+    $photo = $item["image"];
+    echo "<section> <nav><img src='../TinyHR/images/" . $photo . "'></nav>";
+    echo "<article><h1>" . $item["job"] . "<h1>";
+    echo "<p></p></article>";
+    echo "</section><footer></footer>";
+
+}
+?>
+</table>
+</html>
+
