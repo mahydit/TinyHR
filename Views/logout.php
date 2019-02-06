5f7f4fda93da376866364a90cba2442d7c9a5294
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-02-06 10:58:29 +0200

    Signup bug

--- Views/logout.php
+++ Views/logout.php
@@ -1,8 +1,7 @@
 <?php
 // require_once('../autoload.php'); //for debugging
 // session_start();
-if (isset($_SESSION["user_id"]))
-{
+if (isset($_SESSION["user_id"])) {
     $logout = new UserOperations();
     $logout->logout($_SESSION["user_id"]);
     unset($_SESSION["user_id"]);
@@ -12,4 +11,5 @@ if (isset($_SESSION["user_id"]))
 // (isset($_SESSION["is_admin"]))?unset($_SESSION["is_admin"]);//REVIEW:
 session_destroy();
 // header("Location: index.php");
-require_once "../index.php";
+header("Location: http://localhost/TinyHR/index.php");
+// require_once "../index.php";
--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-define("__HOST__","localhost");
-define("__USER__","root");
-define("__PASS__","");
-define("__DB__","tiny-hr");
-define("__DEBUG_MODE__",1);
-define("__USER_TABLE__","user");
+define("__HOST__", "localhost");
+define("__USER__", "root");
+define("__PASS__", "");
+define("__DB__", "tiny-hr");
+define("__DEBUG_MODE__", 1);
+define("__USER_TABLE__", "user");
 define("__RECORD_PER_PAGE__", 5);
 define("__PRIMARY_KEY__", "user_id");
