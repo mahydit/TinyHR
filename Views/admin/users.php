3b5aef43130508d100fe3c8a0351e13650485dcb
mariageorgeamin <46920076+mariageorgeamin@users.noreply.github.com>
2019-02-08 20:12:51 +0200

    added online users

--- Views/admin/users.php
+++ Views/admin/users.php
@@ -18,19 +18,8 @@ $admin = new Admin();
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
     crossorigin="anonymous">
 
-  <title>Users Catalog</title>
 
-  <style>
-    table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-    /* background-color: #f5f5f5; */
-  }
-  
-  
 
-<html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <style>
@@ -98,7 +87,7 @@ a:hover, a:active {
   background: grey;
 }
 
-/* }
+}
     a:link, a:visited {
   background-color: black;
   color: white;
@@ -125,7 +114,7 @@ a:hover, a:active {
 $items = $admin->get_online();
 foreach ($items as $item) {
     echo "<span class='dot'></span>";
-    echo "<span>" . $item['name'] . "</span><br>";
+    echo "<span> " . $item['name'] . "</span><br>";
 }
 ?>
 
@@ -137,79 +126,9 @@ foreach ($items as $item) {
 
 <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
 
- #contact_form input[type=submit] {
-  float: left;
-  width: 10%;
-  padding: 10px;
-  background: black;
-  color: white;
-  font-size: 17px;
-  border: 1px solid grey;
-  border-left: none;
-  cursor: pointer;
-}
-
-#contact_form input[type=submit]:hover {
-  background: grey;
-} */
-
-
-
-  a:link,
-  a:visited {
-    background-color: black;
-    color: white;
-    padding: 14px 25px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-  }
-
-  a:hover,
-  a:active {
-    background-color: grey;
-  }
-
-  #contact_form input[type=text] {
-    padding: 10px;
-    font-size: 17px;
-    border: 1px solid grey;
-    float: left;
-    width: 80%;
-    background: #f1f1f1;
-  }
-
-  #contact_form input[type=submit] {
-    float: left;
-    width: 10%;
-    padding: 10px;
-    background: black;
-    color: white;
-    font-size: 17px;
-    border: 1px solid grey;
-    border-left: none;
-    cursor: pointer;
-  }
-
-  #contact_form input[type=submit]:hover {
-    background: grey;
-  }
-
-  img{
-    height: 70px;
-  }
-
-  /* body{
-    background-color: #f5f5f5;
-  } */
-
-/* div.wrap{
-    width: 80%;
-} */
-
-
-</style>
-
+<input type="text" name="keyword">
+<input type="submit" name ="search" value="Search">
+<input type="submit" name ="showall" value="Show All">
 <table cellspacing="10">
 
 <?php
@@ -234,7 +153,7 @@ echo "<tr>
 foreach ($items as $item) {
     $photo = $item["image"];
     // $src = str_replace(".jpg", "-thumb.jpg", $photo);
-    echo "<tr><td><img src='../TinyHR/images/" . $photo . "'></td>";
+    echo "<tr><td><img src='../TinyHR/images/" . $photo . "' width = 60px height=60px></td>";
     $id = $item["user_id"];
     echo "<td>" . $id . "</td>";
     echo "<td>" . $item["name"] . "</td>";
@@ -244,8 +163,8 @@ foreach ($items as $item) {
 }
 
 if (!(isset($_POST['showall']) || isset($_POST['search']))) {
-  echo "<tr><td colspan=3><a role='button' class='btn btn-dar' href='" . $_SERVER['PHP_SELF'] . "?current=$previous_index'> << Previous  </a></td>";
-  echo "<td colspan=3><a role='button' class='btn btn-dar' href='" . $_SERVER['PHP_SELF'] . "?current=$next_index'> Next >> </a></td></tr>";
+    echo "<tr><td colspan=3><a role='button' class='btn btn-dar' href='" . $_SERVER['PHP_SELF'] . "?current=$previous_index'> << Previous  </a></td>";
+    echo "<td colspan=3><a role='button' class='btn btn-dar' href='" . $_SERVER['PHP_SELF'] . "?current=$next_index'> Next >> </a></td></tr>";
 }
 ?>
 </table>
